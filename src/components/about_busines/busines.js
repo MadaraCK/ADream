53b844c843861d8d3139ca8e2a8d73b202388855
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-05-26 02:51:26 +0200

    zoom

--- src/components/about_busines/busines.js
+++ src/components/about_busines/busines.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import './busines.scss'
+import images from '../../images/Group 146.svg'
 
 function Busines(props) {
     return (
         <section className="container">
+
+
             <div className="first_container">
                 <div className="all_images first_images">
-                    first
+                    <img
+                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=46df58dc-050f-45fe-97ab-85c81f4cc1b5&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
+                        alt=""/>
                 </div>
                 <div className="all_images secondary_images">
-                    secondary
+                    <img
+                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=8e410f61-7e43-49a5-9c10-21f9c420bf51&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
+                        alt=""/>
+
                 </div>
             </div>
             <div className="secondary_container">
                 <div className="all_images third_images">
-                    third
+                    <img
+                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=3a464f82-38b8-440b-8966-9f583c67ecdb&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
+                        alt=""/>
+
                 </div>
                 <div className="all_images fourth_images">
-                    fourth
+                    <img
+                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=75e790e8-1316-41be-b2c6-2821fb14cc1c&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
+                        alt=""/>
                 </div>
             </div>
+
         </section>
     );
 }
