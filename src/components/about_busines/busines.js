6a23a8ccb1c14f89591983daaf068123b45f5aab
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-05-26 17:04:22 +0200

    img, zoom,

--- src/components/about_busines/busines.js
+++ src/components/about_busines/busines.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import './busines.scss'
-import img1 from './imgbusines1.png'
-import img2 from '../images/imgbusines2.png'
-import img3 from '../images/imgbusines3.png'
-import img4 from '../images/imgbusines4.png'
 
-function Busines(props) {
+function Busines() {
     return (
         <section className="container">
             <div className="first_container">
                 <div className="all_images first_images">
-                    <img src={img1} alt="x"/>
+                    <div className="img1">
+                        <div className="box_ofice">
+                            <h2 className="ofice ofice1">Biuro</h2>
+                            <p className="ofice_cost">od 1000zl/msc</p>
+
+                        </div>
+                    </div>
                 </div>
                 <div className="all_images secondary_images">
-                    <img src={img2} alt="x"/>
+                    <div className="img2">
+                        <div className="ofice_lorem">
+                            <h2 className="ofice ofice2">Lorem Ipsum</h2>
+                            <p className="ofice_cost">od 1000zl/msc</p>
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
             <div className="secondary_container">
                 <div className="all_images third_images">
-                    <img src={img3} alt=""/>
+                    <div className="img3">
+                        <div className="ofice_lorem3">
+                            <h2 className="ofice ofice3">Biuro Lorem</h2>
+                            <p className="ofice_cost">od 1000zl/msc</p>
+
+                        </div>
+                    </div>
                 </div>
                 <div className="all_images fourth_images">
-                    <img src={img4} alt=""/>
+                    <div className="img4">
+                        <div className="ofice_lorem4">
+                            <h2 className="ofice ofice4">Biuro Lorem ipsum Lorem</h2>
+                            <p className="ofice_cost">od 1000zl/msc</p>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
