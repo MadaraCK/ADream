da3af9f6f62ee026480b10b5d1b07307133683cd
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-05-26 15:35:42 +0200

    img

--- src/components/about_busines/busines.js
+++ src/components/about_busines/busines.js
@@ -1,39 +1,29 @@
 import React from 'react';
 import './busines.scss'
-import images from '../../images/Group 146.svg'
+import img1 from './imgbusines1.png'
+import img2 from '../images/imgbusines2.png'
+import img3 from '../images/imgbusines3.png'
+import img4 from '../images/imgbusines4.png'
 
 function Busines(props) {
     return (
         <section className="container">
-
-
             <div className="first_container">
                 <div className="all_images first_images">
-                    <img
-                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=46df58dc-050f-45fe-97ab-85c81f4cc1b5&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
-                        alt=""/>
+                    <img src={img1} alt="x"/>
                 </div>
                 <div className="all_images secondary_images">
-                    <img
-                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=8e410f61-7e43-49a5-9c10-21f9c420bf51&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
-                        alt=""/>
-
+                    <img src={img2} alt="x"/>
                 </div>
             </div>
             <div className="secondary_container">
                 <div className="all_images third_images">
-                    <img
-                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=3a464f82-38b8-440b-8966-9f583c67ecdb&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
-                        alt=""/>
-
+                    <img src={img3} alt=""/>
                 </div>
                 <div className="all_images fourth_images">
-                    <img
-                        src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:3ac525a0-27cc-4beb-a58b-1dff8306cc5c;revision=0?component_id=75e790e8-1316-41be-b2c6-2821fb14cc1c&api_key=CometServer1&access_token=1653553975_urn%3Aaaid%3Asc%3AUS%3A3ac525a0-27cc-4beb-a58b-1dff8306cc5c%3Bpublic_96d10280c69a233fa062859c1da78a0aeaf4cacf"
-                        alt=""/>
+                    <img src={img4} alt=""/>
                 </div>
             </div>
-
         </section>
     );
 }
